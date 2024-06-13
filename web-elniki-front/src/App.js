631aa693c6c2c12a5c355aa2c48bf0329914df0a
NellKree <oneevgexaone@gmail.com>
2024-06-13 23:56:21 +0500

    Info pages first commit

--- web-elniki-front/src/App.js
+++ web-elniki-front/src/App.js
@@ -1,4 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './styles/main.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Developedoffer from './pages/Developedoffer';
+
 function App() {
+  return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/services.html" element={<Developedoffer />} />
+
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+  );
 }
 
 export default App;
---
+++ web-elniki-front/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Header = () => {
+    return (
+		<header className="header">
+			<div className="header__wrapper">
+				<h1 className="header__title">
+					<strong>
+						Добро пожаловать в  <em>Ельники</em>
+					</strong>
+				</h1>
+				<div className="header__text">
+					<p>Лучшие истории начинаются здесь</p>
+				</div>
+				<a href="./services.html" className="btn">
+					Услуги
+				</a>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
