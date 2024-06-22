2f982c07a4cad89cad4859094b0bbbb233064678
dariamm03 <osa.m@mail.ru>
2024-06-22 13:50:09 +0500

    backend

--- web-elniki-front/src/App.js
+++ web-elniki-front/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
 import './styles/main.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Home from './pages/Home';
 import Developedoffer from './pages/Developedoffer';
-import About from './pages/About';
-import Contact from './pages/Contact';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { OrderPage } from "./orderBuyingComponents/OrderPage";
 import { BuyingPage } from "./orderBuyingComponents/BuyingPage";
@@ -28,10 +24,6 @@ function App() {
           <Route path="/services.html" element={<Developedoffer />} />
           <Route path='/login' element={<LogPage />}/>
           <Route path='/reg' element={<RegPage />}/>
-	<Route path="/accommodation" element={<div>Accommodation Page</div>} />
-          <Route path="/abouttheresort" element={<About/>} />
-          <Route path="/account" element={<div>Account Page</div>} />
-          <Route path="/contact" element={<Contact/>} />
 				</Routes>
 				<Footer />
 			</Router>
