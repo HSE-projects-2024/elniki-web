7c809bb086b85c183ffefa4ed96ed406fd4f75c0
Daria Rakovskaia <rakovskaiadaria03@gmail.com>
2024-06-22 18:36:22 +0500

    reg user

--- web-elniki-back/routes/user.js
+++ web-elniki-back/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../db');
+const bcrypt = require('bcrypt');
 
 // GET запрос для получения всех пользователей
-router.get('/users', async (req, res) => {
+router.get('/users', (req, res) => {
     conn.query('SELECT * FROM users', (err, result) => {
         if (err) {
             console.error('Ошибка при получении пользователей:', err);
@@ -12,6 +13,28 @@ router.get('/users', async (req, res) => {
         }
         res.json(result);
     });
+});
+
+// POST запрос для регистрации пользователя
+router.post('/reg', async (req, res) => {
+    var {
+        Name,
+        Surname,
+        Email,
+        Password
+    } = req.body;
+    Password = await bcrypt.hash(Password, 10);
+    console.log(Password)
+    conn.query(
+        'INSERT INTO users (Name, Surname, Email, Password) VALUES (?, ?, ?, ?)',
+        [Name, Surname, Email, Password], (err, result) => {
+            if (err) {
+                console.error('Ошибка при регистрации пользователя:', err);
+                res.status(500).json({ error: 'Ошибка сервера' });
+                return;
+            }
+            res.json({ Email, Password });
+        });
 });
 
 module.exports = router;
