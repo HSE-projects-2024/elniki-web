bdb9b24331206aafdeefb2bcecf54b258426bda4
Daria Rakovskaia <rakovskaiadaria03@gmail.com>
2024-06-22 17:50:42 +0500

    GET users

---
+++ web-elniki-back/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const userRouter = require('./routes/user');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', userRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
