cc1945993866d88f1301eef1e740a48d9e263adc
Daria Rakovskaia <rakovskaiadaria03@gmail.com>
2024-06-23 02:24:42 +0500

    changes for testing

--- web-elniki-back/index.js
+++ web-elniki-back/index.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', userRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001;
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = app;
+module.exports = { app, server };
--- web-elniki-back/routes/user.js
+++ web-elniki-back/routes/user.js
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
 
             if (isValidPassword) {
                 const token = jwt.sign({ userId: user.UserID }, process.env.JWT_SECRET, { expiresIn: '7d' });
-                return res.json({ token });
+                return res.json({ jwtToken: token });
             } else {
                 return res.status(401).json({ message: 'Неверные данные' });
             }
